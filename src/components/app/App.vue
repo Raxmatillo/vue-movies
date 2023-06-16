87d214052b5a2eb7ad1d5ff4c977a000f016a36f
Raxmatillo <milodcomposer@gmail.com>
2023-06-16 19:42:30 +0500

    initial commit

---
+++ src/components/app/App.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="app font-monospace">
+    <div class="content">
+      <AppInfo :allMoviesCount='movies.length' :favouriteMoviesCount="movies.filter(c => c.favourite).length"/>
+      <div class="search-panel">
+        <SearchPanel />
+        <AppFilter />
+      </div>
+      <MovieList :movies="movies" />
+      <MovieAddForm @createMovie="createMovie"/>
+    </div>
+  </div>
+</template>
+
+
+
+<script>
+import AppInfo from "../app-info/AppInfo.vue";
+import SearchPanel from "../search-panel/SearchPanel.vue";
+import AppFilter from "../app-filter/AppFilter.vue"
+import MovieList from "../movie-list/MovieList.vue"
+import MovieAddForm from "../movia-add-form/MovieAddForm.vue"
+
+export default {
+  components: {
+    AppInfo,
+    SearchPanel,
+    AppFilter,
+    MovieList,
+    MovieAddForm
+  },
+  data() {
+        return{
+            movies:[
+                {   
+                    name: "Harry Potter",
+                    viewers: 811,
+                    favourite: false,
+                    like: true,
+                    id: 1
+                },
+                {
+                    name: "Doctor House",
+                    viewers: 483,
+                    favourite: true,
+                    like: true,
+                    id: 2
+                },
+                {
+                    name: "Sherlock Holmes",
+                    viewers: 923,
+                    favourite: true,
+                    like: false,
+                    id: 3
+                },
+            ],
+        }
+    },
+    methods: {
+      createMovie(item) {
+        this.movies.push(item)
+      }
+    }
+}
+</script>
+
+
+<style>
+.app{
+  height: 100vh;
+  color: #000;
+}
+
+.content {
+  width: 1000px;
+  min-height: 700px;
+  background-color: #fff;
+  margin: 0 auto;
+  padding: 5rem 0;
+}
+
+.search-panel {
+  margin-top: 2rem;
+  padding: 1.5rem;
+  background-color: #fcfaf5;
+  border-radius: 4px;
+  box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
+}
+</style>
---
+++ src/components/movie-list/MovieList.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul  class="movie-list list-group">
+        <MovieListItem v-for="movie in movies" v-bind:movie="movie" :key="movie.id"/>
+    </ul>
+</template>
+
+<script>
+import MovieListItem from "../movie-list-item/MovieListItem.vue"
+
+export default {
+    components: {MovieListItem},
+    props: {
+        movies: {
+            type: Array,
+            required: true,
+            
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+.movie-list{
+  margin-bottom: 1rem;
+  padding: 1.5rem;
+  background-color: #fcfaf5;
+  border-radius: 4px;
+  box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
+}
+</style>
